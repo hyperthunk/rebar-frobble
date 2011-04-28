836d85410403221b00acffb852db8c7b4bc8637f
Tim Watson <watson.timothy@gmail.com>
2011-04-28 12:51:50 +0100

    Make plugin post-hook the compile command as well

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Example of using rebar plugins to customise your build
 
 Run `rebar clean compile frobble -v` to see it working.
-
--- build/src/rebar_frobbler.erl
+++ build/src/rebar_frobbler.erl
@@ -1,7 +1,10 @@
 -module(rebar_frobbler).
 
 %% Application callbacks
--export([frobble/2]).
+-export([compile/2, frobble/2]).
 
-frobble(Config, _) ->
-    rebar_log:log(warn, "This Plugin is about to FROBBLE your config: [~p]~n", [Config]).
+compile(Config, X) ->
+    rebar_log:log(warn, "This Plugin is post-processing the compile command: [~p][~p]~n", [X, Config]).
+
+frobble(Config, X) ->
+    rebar_log:log(warn, "This Plugin is about to FROBBLE your config: [~p][~p]~n", [X, Config]).
